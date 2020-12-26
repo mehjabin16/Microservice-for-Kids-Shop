58f4efa0d2d6acef0e6a4b847c0313262c80c085
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2020-12-27 05:28:14 +0600

    service to service call done

--- RatingService/functions/index.js
+++ RatingService/functions/index.js
@@ -1,3 +1,5 @@
+const apisauce = require('apisauce');
+
 const functions = require('firebase-functions');
 var admin = require("firebase-admin");
 
@@ -6,11 +8,13 @@ var serviceAccount = require("./permission.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+ 
+// define the api
+const api = apisauce.create({
+  baseURL: 'http://localhost:51349/',
+  
+})
 
-var sslRootCAs = require('ssl-root-cas/latest')
-sslRootCAs.inject()
-
-const request = require('request');
 
 const express = require('express');
 const app = express();
@@ -24,27 +28,6 @@ app.get('/hello-world', (req,res) =>{
     return res.status(200).send("hello world!");
 })
 
-app.get('/products', (req,res) =>{
-    (  async()=>{
-        try{
-            request({
-                "rejectUnauthorized": false,
-                "url": "https://localhost:44355/product/list",
-                "method": "GET",
-                
-            }, function(err, response, body){
-                //console.log(err);
-                //console.log(response);
-                console.log(body);
-                return res.status(201).send(body)
-            });
-        }
-        catch(error){
-            return res.status(500).send(error)
-        }
-    })();
-});
-   
     
 app.post('/rate', (req,res) =>{
     (async()=>{
@@ -76,57 +59,40 @@ app.post('/rate', (req,res) =>{
                     }
                 )                  
             } 
+
             const ratingdata = await ratingRef.where('productID', '==', req.body.productID)
             .get();
             var total_rating = 0;
             var avg_rating = 0;
             var productID = req.body.productID;
             ratingdata.forEach(doc => {
-            console.log(doc.data().rating);
+            //console.log(doc.data().rating);
                 total_rating = total_rating+doc.data().rating;
             });
             avg_rating = total_rating/ratingdata.size;
             //console.log(avg_rating);
 
-            let response = [];
-            const rating={
-                productID:productID,
-                numberOfRaters: ratingdata.size,
-                avg_rating: avg_rating
+            const alldata = await ratingRef.get();
+            //console.log(alldata.size)
+            sizemod = alldata.size%5
+
+            if(sizemod==0)
+            { 
+                alldata.forEach(async(item) => {
+                let update = await api.post('product/updateRating', {  
+
+                    "ProductID": productID,
+                    "AverageRating": avg_rating,
+                    "NumberOfRaters":  ratingdata.size               
+                
+                }) 
+                //console.log(avg_rating)
+                res.status(200).send(update)
+            })}
+            else{
+                console.log("Not updated");
+                res.status(200).send()
             }
-            response.push(rating)
-
-            
-            return res.status(201).send(response)     
-
-        }
-        catch(error){
-            return res.status(500).send(error)
-        }
-    })();
-});
-
-
-
-
-app.get('/ratinginfo', (req,res) =>{
-    (async()=>{
-        try{
-            const ratingRef =  db.collection('rating').doc(4)
-            let response =[] ;
-            await ratingRef.get().then(querySnapshot =>{
-                let docs=querySnapshot.docs;
-                for(let doc of docs){
-                    const ratingList={
-                        productid:doc.data().productID,
-                        rating:doc.data().rating,
-                    }
-                    response.push(ratingList)
-                }
-                return response;
-            }) 
-            return res.status(201).send(response)
-
         }
         catch(error){
             return res.status(500).send(error)
